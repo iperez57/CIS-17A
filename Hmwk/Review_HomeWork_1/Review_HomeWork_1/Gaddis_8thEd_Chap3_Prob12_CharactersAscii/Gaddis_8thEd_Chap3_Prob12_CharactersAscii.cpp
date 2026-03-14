25693d96ad4f35b269756b8858577af38260d2d5
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-14 12:53:39 -0700

    displays the character's ascii number

--- Hmwk/Review_HomeWork_1/Review_HomeWork_1/Gaddis_8thEd_Chap3_Prob12_CharactersAscii/Gaddis_8thEd_Chap3_Prob12_CharactersAscii.cpp
+++ Hmwk/Review_HomeWork_1/Review_HomeWork_1/Gaddis_8thEd_Chap3_Prob12_CharactersAscii/Gaddis_8thEd_Chap3_Prob12_CharactersAscii.cpp
@@ -1,13 +1,41 @@
+/*
+* File Name: Gaddis_8thEd_Chap3_Prob12_CharactersAscii
+* Author: Isaac Perez
+* Created: 03/14/2026
+* Purpose: Display characters Ascii
+*/
 
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
 
-#include <iostream> 
-using namespace std; 
+//User Libraries
 
-int main()
-{
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
 
-	char letter;
+//Function Prototypes
 
-	// Finish this program // as specified above, return 0;
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
 
+    //Declare all variables for this function
+    char letter;
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    cout << "Enter a letter: ";
+    cin >> letter;
+
+    cout << "The ASCII character for " << letter << " is " << int(letter);
+   
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
 }
+
+//Function Implementations
