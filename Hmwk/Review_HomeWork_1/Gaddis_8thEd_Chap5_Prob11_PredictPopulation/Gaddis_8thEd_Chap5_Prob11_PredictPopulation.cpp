da8b7d4458910568a75fadf6c5bbed85fbdbd363
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-15 11:22:53 -0700

    skeleton for 5.11 homework

---
+++ Hmwk/Review_HomeWork_1/Gaddis_8thEd_Chap5_Prob11_PredictPopulation/Gaddis_8thEd_Chap5_Prob11_PredictPopulation.cpp
@@ -0,0 +1,37 @@
+/*
+ * File:   Gaddis_8thEd_Chap5_Prob11_PredictPopulation
+ * Author: Isaac Perez
+ * Created: 03/15/2026
+ * Purpose:  Predict the size of an organisms population.
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
