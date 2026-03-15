74ebeeb9736f679166647db55bc2bad012a6bda9
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-15 15:58:14 -0700

    skeleton for 8.7 homework

---
+++ Hmwk/Review_HomeWork_1/Gaddis_8thEd_Chap8_Prob7_SearchBenchmarks/Gaddis_8thEd_Chap8_Prob7_SearchBenchmarks.cpp
@@ -0,0 +1,41 @@
+/*
+ * File:   Gaddis_8thEd_Chap8_Prob7_SearchBenchmarks
+ * Author: Isaac Perez
+ * Created: 03/15/2026
+ * Purpose:   search an array using linear search and binary search
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <fstream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+    
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Read external file
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
