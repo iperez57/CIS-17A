9e6d1e0dac7d6fe7662a5409118d46dadaeaab08
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-15 11:12:30 -0700

    checks if year is a leap year then displays days in month

--- Hmwk/Review_HomeWork_1/Gaddis_8thEd_Chap4_Prob10_DaysInMonth/Gaddis_8thEd_Chap4_Prob10_DaysInMonth.cpp
+++ Hmwk/Review_HomeWork_1/Gaddis_8thEd_Chap4_Prob10_DaysInMonth/Gaddis_8thEd_Chap4_Prob10_DaysInMonth.cpp
@@ -15,7 +15,7 @@ using namespace std;
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-
+int dayInMonth(int month ,int year);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -23,15 +23,49 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    int month;
+    int year;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    cout << "Enter a month (1-12): ";
+    cin >> month;
+    cout << "Enter a year: ";
+    cin >> year;
+    cout << dayInMonth(month, year) << " days";
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
 }
 
 //Function Implementations
+
+int dayInMonth(int month, int year)
+{
+    int days;
+        switch (month)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            days = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            days = 30;
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                days = 29;
+            else 
+                days = 28;
+        }
+        return days;
+}
