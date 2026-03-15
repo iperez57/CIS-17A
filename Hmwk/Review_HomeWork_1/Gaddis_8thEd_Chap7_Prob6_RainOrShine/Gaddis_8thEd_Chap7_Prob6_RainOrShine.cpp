9ac920b198195db4625c9d4940193501f8fd7364
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-15 12:18:51 -0700

    skeleton for rain or shine

---
+++ Hmwk/Review_HomeWork_1/Gaddis_8thEd_Chap7_Prob6_RainOrShine/Gaddis_8thEd_Chap7_Prob6_RainOrShine.cpp
@@ -0,0 +1,38 @@
+/*
+ * File:   Gaddis_8thEd_Chap7_Prob6_RainOrShine
+ * Author: Isaac Perez
+ * Created: 03/15/2026
+ * Purpose:     using an array to display characters
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+// 
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+    // 
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
+
