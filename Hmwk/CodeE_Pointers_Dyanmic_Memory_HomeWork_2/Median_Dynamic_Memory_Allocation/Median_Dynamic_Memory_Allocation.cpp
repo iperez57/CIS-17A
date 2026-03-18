f25c0b29236be10b459c5e26ff1b46dfde7e09a8
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-18 11:59:35 -0700

    Skeleton for Median - Dynamic Memory Allocation

---
+++ Hmwk/CodeE_Pointers_Dyanmic_Memory_HomeWork_2/Median_Dynamic_Memory_Allocation/Median_Dynamic_Memory_Allocation.cpp
@@ -0,0 +1,41 @@
+/*
+ * File:   Median_Dynamic_Memory_Allocation
+ * Author: Isaac Perez
+ * Created: 03/18/2026
+ * Purpose:  Find the median 
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+int* getData(int&);         //Return the array size and the array
+void prntDat(int*, int);    //Print the integer array
+float* median(int*, int);  //Fill the median Array with the Float array size, the median, and the integer array data
+void prntMed(float*);     //Print the median Array
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    // 
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
