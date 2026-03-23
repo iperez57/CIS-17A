fcc0778691d795ea9e4a9778d83f469bd535de72
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-22 20:03:52 -0700

    skeleton for 1D augment dynamic memory allocation homeowork

---
+++ Hmwk/CodeE_Pointers_Dyanmic_Memory_HomeWork_2/1D_Augment_Dynamic_Memory_Allocation/1D_Augment_Dynamic_Memory_Allocation.cpp
@@ -0,0 +1,40 @@
+/*
+ * File:   1D_Augment_Dynamic_Memory_Allocation
+ * Author: Isaac Perez
+ * Created: 03/22/2026
+ * Purpose: Augment a 1D array
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+int* getData(int&);
+int* augment(const int*, int);
+void prntAry(const int*, int);
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
