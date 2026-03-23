6cf2b3a4ca935870ec2fa9aa00d88cef86daf6c7
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-22 18:34:33 -0700

    skeleton for 2D sum dynamic memory allocaiton

---
+++ Hmwk/CodeE_Pointers_Dyanmic_Memory_HomeWork_2/2D_Sum_Dynamic_Memory_Allocation/2D_Sum_Dynamic_Memory_Allocation.cpp
@@ -0,0 +1,43 @@
+/*
+ * File:   2D_Sum_Dynamic_Memory_Allocation
+ * Author: Isaac Perez
+ * Created: 03/22/2026
+ * Purpose:  2D sum of an array
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+int** getData(int&, int&);
+void prntData(const int* const*, int, int);
+void destroy(int**, int, int);
+int sum(const int* const*, int, int);
+
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
