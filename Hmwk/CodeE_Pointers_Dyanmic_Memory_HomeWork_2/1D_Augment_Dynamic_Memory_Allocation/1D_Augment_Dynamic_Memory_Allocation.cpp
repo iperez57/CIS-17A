828f0770220f39326dee82ce90a04695be3c97d1
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-03-24 11:20:42 -0700

    cleans up memory

--- Hmwk/CodeE_Pointers_Dyanmic_Memory_HomeWork_2/1D_Augment_Dynamic_Memory_Allocation/1D_Augment_Dynamic_Memory_Allocation.cpp
+++ Hmwk/CodeE_Pointers_Dyanmic_Memory_HomeWork_2/1D_Augment_Dynamic_Memory_Allocation/1D_Augment_Dynamic_Memory_Allocation.cpp
@@ -42,7 +42,8 @@ int main(int argc, char** argv) {
 
 
     //Clean up the code, close files, deallocate memory, etc....
-
+    delete[]ary;
+    delete[]augArry;
     //Exit stage right
     return 0;
 }
