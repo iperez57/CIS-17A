026d9b242bb2eca27824ad181116374666fdc9da
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-15 11:25:23 -0700

    created outline for Ga9EdC11P11

---
+++ Hmwk/Code_E_Structures/Ga9EdC11P11/Ga9EdC11P11.cpp
@@ -0,0 +1,56 @@
+/*
+ * File:   Ga9EdC11P11
+ * Author: Isaac Perez
+ * Created: 04/15/2026
+ * Purpose:  Use structures
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+using namespace std;
+
+//User Libraries
+struct MonthlyBudget
+{
+    float housing;
+    float utilities;
+    float householdExpenses;
+    float transportation;
+    float food;
+    float medical;
+    float insurance;
+    float entertainment;
+    float clothing;
+    float miscellaneious;
+
+};
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+void input(MonthlyBudget m);
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+    MonthlyBudget m;
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
+void input(MonthlyBudget m)
+{
+
+}
