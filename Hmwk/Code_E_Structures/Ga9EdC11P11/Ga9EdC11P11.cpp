d88a379b891e4ee8cca272ff28e9b8d6f20b28a5
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-15 12:29:37 -0700

    calculates over and under budget as well as if total budget is over or under.

--- Hmwk/Code_E_Structures/Ga9EdC11P11/Ga9EdC11P11.cpp
+++ Hmwk/Code_E_Structures/Ga9EdC11P11/Ga9EdC11P11.cpp
@@ -7,6 +7,7 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include <iomanip>
 
 using namespace std;
 
@@ -22,36 +23,39 @@ struct MonthlyBudget
     float insurance = 100.00;
     float entertainment = 150.00;
     float clothing = 75.00;
-    float miscellaneious = 50.00;
+    float miscellaneous = 50.00;
 
 };
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-void input(MonthlyBudget m);
+MonthlyBudget input(MonthlyBudget m);
+void ReportIndication(MonthlyBudget m, MonthlyBudget budget);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
+    MonthlyBudget budget;
     MonthlyBudget m;
-
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-    input(m);
+    m =input(m);
+    ReportIndication(m, budget);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
 }
 
 //Function Implementations
-void input(MonthlyBudget m)
+MonthlyBudget input(MonthlyBudget m)
 {
+    
     cout << "Enter housing cost for the month:$" << endl;
     cin >> m.housing;
     cout << "Enter utilities cost for the month:$" << endl;
@@ -71,5 +75,170 @@ void input(MonthlyBudget m)
     cout << "Enter clothing cost for the month:$" << endl;
     cin >> m.clothing;
     cout << "Enter miscellaneous cost for the month:$" << endl;
-    cin >> m.miscellaneious;
+    cin >> m.miscellaneous;
+
+    return m;
+}
+
+void ReportIndication(MonthlyBudget m, MonthlyBudget budget)
+{
+    //housing comparison
+    if (m.housing == budget.housing)
+    {
+        cout << "Housing Even" << endl;
+    }
+    else if (m.housing > budget.housing)
+    {
+        cout << "Housing Over" << endl;
+    }
+    else
+    {
+        cout << "Housing Under" << endl;
+    }
+
+    //Utilities comparison
+    if (m.utilities == budget.utilities)
+    {
+        cout << "Utilities Even" << endl;
+    }
+    else if (m.utilities > budget.utilities)
+    {
+        cout << "Utilities Over" << endl;
+    }
+    else
+    {
+        cout << "Utilities Under" << endl;
+    }
+
+    //Household comparison
+    if (m.householdExpenses == budget.householdExpenses)
+    {
+        cout << "Household Expenses Even" << endl;
+    }
+    else if (m.householdExpenses > budget.householdExpenses)
+    {
+        cout << "Household Expenses Over" << endl;
+    }
+    else
+    {
+        cout << "Household Expenses Under" << endl;
+    }
+
+    //Transportation comparison
+    if (m.transportation == budget.transportation)
+    {
+        cout << "Transportation Even" << endl;
+    }
+    else if (m.transportation > budget.transportation)
+    {
+        cout << "Transportation Over" << endl;
+    }
+    else
+    {
+        cout << "Transportation Under" << endl;
+    }
+
+    //Food comparison
+    if (m.food == budget.food)
+    {
+        cout << "Food Even" << endl;
+    }
+    else if (m.food > budget.food)
+    {
+        cout << "Food Over" << endl;
+    }
+    else
+    {
+        cout << "Food Under" << endl;
+    }
+
+    //Medical comparison
+    if (m.medical == budget.medical)
+    {
+        cout << "Medical Even" << endl;
+    }
+    else if (m.medical > budget.medical)
+    {
+        cout << "Medical Over" << endl;
+    }
+    else
+    {
+        cout << "Medical Under" << endl;
+    }
+
+    //Insurance comparison
+    if (m.insurance == budget.insurance)
+    {
+        cout << "Insurance Even" << endl;
+    }
+    else if (m.insurance > budget.insurance)
+    {
+        cout << "Insurance Over" << endl;
+    }
+    else
+    {
+        cout << "Insurance Under" << endl;
+    }
+
+    //Entertainment comparison
+    if (m.entertainment == budget.entertainment)
+    {
+        cout << "Entertainment Even" << endl;
+    }
+    else if (m.entertainment > budget.entertainment)
+    {
+        cout << "Entertainment Over" << endl;
+    }
+    else
+    {
+        cout << "Entertainment Under" << endl;
+    }
+
+    //Clothing comparison
+    if (m.clothing == budget.clothing)
+    {
+        cout << "Clothing Even" << endl;
+    }
+    else if (m.clothing > budget.clothing)
+    {
+        cout << "Clothing Over" << endl;
+    }
+    else
+    {
+        cout << "Clothing Under" << endl;
+    }
+
+    //Miscellaneous comparison
+    if (m.miscellaneous == budget.miscellaneous)
+    {
+        cout << "Miscellaneous Even" << endl;
+    }
+    else if (m.miscellaneous > budget.miscellaneous)
+    {
+        cout << "Miscellaneous Over" << endl;
+    }
+    else
+    {
+        cout << "Miscellaneous Under" << endl;
+    }
+    
+    //Total budget comparison
+    float totalM = m.housing + m.utilities + m.householdExpenses + m.transportation + m.food
+        + m.medical + m.insurance + m.entertainment + m.clothing + m.miscellaneous;
+    float totalB = budget.housing + budget.utilities + budget.householdExpenses + budget.transportation + budget.food
+        + budget.medical + budget.insurance + budget.entertainment + budget.clothing + budget.miscellaneous;
+    
+    if (totalM == totalB)
+    {
+        cout << "Your budget was even" << endl;
+    }
+    else if (totalM > totalB)
+    {
+        cout << fixed << setprecision(2) << "You were $" << totalM - totalB << " over budget" << endl;
+    }
+    else
+    {
+        cout << fixed << setprecision(2) << "You were $" << totalB - totalM << " under budget";
+    }
+
 }
