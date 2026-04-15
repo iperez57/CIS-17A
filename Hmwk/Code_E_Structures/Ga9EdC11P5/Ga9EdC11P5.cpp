c690a5441139b1fba81d2abae354e9a911341d09
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-14 18:14:06 -0700

    asks for monthly rainfall, highest temp, and lowest temp

--- Hmwk/Code_E_Structures/Ga9EdC11P5/Ga9EdC11P5.cpp
+++ Hmwk/Code_E_Structures/Ga9EdC11P5/Ga9EdC11P5.cpp
@@ -11,7 +11,13 @@
 using namespace std;
 
 //User Libraries
-enum months{January, February, March, April, May, June, July, August, September, October, November, December};
+struct MonthInfo
+{
+    enum months { January, February, March, April, May, June, July, August, September, October, November, December };
+    float rainfall = 0;
+    float hTemp = 0;
+    float lTemp = 0;
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
@@ -22,14 +28,26 @@ int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
-
+    MonthInfo m[6];
+    float avgRain = 0;
+    float highTemp = 0;
+    float lowTemp = 200;
+    float avgTemp = 0;
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    for (int i = 0; i < 6; i++)
+    {
+        cout << "Enter the total rainfall for the month:" << endl;
+        cin >> m[i].rainfall;
+        cout << "Enter the high temp:" << endl;
+        cin >> m[i].hTemp;
+        cout << "Enter the low temp:" << endl;
+        cin >> m[i].lTemp;
+    }
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
