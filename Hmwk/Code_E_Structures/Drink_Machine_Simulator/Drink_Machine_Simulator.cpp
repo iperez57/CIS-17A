e62e8877122f1dee358cdd2647a339fb70c91383
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-16 11:18:42 -0700

    displays all vending machine information and quit option in a format

--- Hmwk/Code_E_Structures/Drink_Machine_Simulator/Drink_Machine_Simulator.cpp
+++ Hmwk/Code_E_Structures/Drink_Machine_Simulator/Drink_Machine_Simulator.cpp
@@ -7,14 +7,15 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include <iomanip>
 
 using namespace std;
 
 //User Libraries
-struct SoftDrink
+struct VendingMachine
 {
     string name;
-    float cost;
+    int cost;
     int numOfDrink;
 
 };
@@ -22,7 +23,7 @@ struct SoftDrink
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-void display(SoftDrink drinks[]);
+void display(VendingMachine drinks[]);
 
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
@@ -31,34 +32,42 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-    SoftDrink drinks[5];
+    VendingMachine drinks[5];
     drinks[0].name = "Cola";
-    drinks[0].cost = .75;
+    drinks[0].cost = 75;
     drinks[0].numOfDrink = 20;
 
     drinks[1].name = "Root Beer";
-    drinks[1].cost = .75;
+    drinks[1].cost = 75;
     drinks[1].numOfDrink = 20;
 
     drinks[2].name = "Lemon-Lime";
-    drinks[2].cost = .75;
+    drinks[2].cost = 75;
     drinks[2].numOfDrink = 20;
 
     drinks[3].name = "Grape Soda";
-    drinks[3].cost = .80;
+    drinks[3].cost = 80;
     drinks[3].numOfDrink = 20;
 
     drinks[4].name = "Cream Soda";
-    drinks[4].cost = .80;
+    drinks[4].cost = 80;
     drinks[4].numOfDrink = 20;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
     
     //Display the Inputs/Outputs
-    void display(SoftDrink drinks[5]);
+    display(drinks);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
 }
 
 //Function Implementations
+void display(VendingMachine drinks[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << left << setw(11) << drinks[i].name << setw(4) << drinks[i].cost << drinks[i].numOfDrink << endl;
+    }
+    cout << "Quit" << endl;
+}
