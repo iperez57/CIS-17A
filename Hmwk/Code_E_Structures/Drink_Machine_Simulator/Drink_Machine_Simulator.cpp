b2ccd115475adee9cc48b1786a0d7a0a2b0d39d0
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-16 10:52:18 -0700

    initialized all drink names, cost, and amount of drinks

--- Hmwk/Code_E_Structures/Drink_Machine_Simulator/Drink_Machine_Simulator.cpp
+++ Hmwk/Code_E_Structures/Drink_Machine_Simulator/Drink_Machine_Simulator.cpp
@@ -22,7 +22,8 @@ struct SoftDrink
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-// 
+void display(SoftDrink drinks[]);
+
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -30,12 +31,31 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
+    SoftDrink drinks[5];
+    drinks[0].name = "Cola";
+    drinks[0].cost = .75;
+    drinks[0].numOfDrink = 20;
 
+    drinks[1].name = "Root Beer";
+    drinks[1].cost = .75;
+    drinks[1].numOfDrink = 20;
+
+    drinks[2].name = "Lemon-Lime";
+    drinks[2].cost = .75;
+    drinks[2].numOfDrink = 20;
+
+    drinks[3].name = "Grape Soda";
+    drinks[3].cost = .80;
+    drinks[3].numOfDrink = 20;
+
+    drinks[4].name = "Cream Soda";
+    drinks[4].cost = .80;
+    drinks[4].numOfDrink = 20;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
-
+    
     //Display the Inputs/Outputs
-
+    void display(SoftDrink drinks[5]);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
