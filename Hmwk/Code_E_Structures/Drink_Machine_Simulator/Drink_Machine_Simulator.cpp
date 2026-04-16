825c139ccd88ae1eb1b0a0ca542d02295ac63471
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-16 10:42:29 -0700

    outline for drink machine simulator

---
+++ Hmwk/Code_E_Structures/Drink_Machine_Simulator/Drink_Machine_Simulator.cpp
@@ -0,0 +1,44 @@
+/*
+ * File:   Drink_Machine_Simulator
+ * Author: Isaac Perez
+ * Created: 04/16/2026
+ * Purpose:  Use structures
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+using namespace std;
+
+//User Libraries
+struct SoftDrink
+{
+    string name;
+    float cost;
+    int numOfDrink;
+
+};
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+// 
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
