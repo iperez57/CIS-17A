958735b69f08857f8074e0d3962f16bd31149509
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-17 19:36:29 -0700

    displays the menu to the user

--- Hmwk/Code_E_Structures/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau.cpp
+++ Hmwk/Code_E_Structures/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau.cpp
@@ -11,25 +11,32 @@
 using namespace std;
 
 //User Libraries
-
+struct Speaker
+{
+    string name;
+    string phoneNumber;
+    string speakingTopic;
+    float fee;
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-
+void displayMenu(Speaker speakersBureau[], int arySize);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
-
+    const int arySize = 10;
+    Speaker speakersBureau[arySize];
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    displayMenu(speakersBureau, arySize);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
 
@@ -37,3 +44,10 @@ int main(int argc, char** argv) {
 }
 
 //Function Implementations
+void displayMenu(Speaker speakersBureau[], int arySize)
+{
+    cout << "1. Add Speaker" << endl;
+    cout << "2. Edit Speaker" << endl;
+    cout << "3. Display All" << endl;
+    cout << "4. Exit" << endl;
+}
