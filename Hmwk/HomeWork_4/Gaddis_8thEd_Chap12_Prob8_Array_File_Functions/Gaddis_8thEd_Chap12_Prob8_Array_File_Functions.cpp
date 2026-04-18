547b3fc1a43611a2facc3d58415473fa8f30f0cb
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-18 13:48:56 -0700

    outline for ch12 prob 8 array file functions

---
+++ Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions.cpp
@@ -0,0 +1,42 @@
+/*
+ * File:   Gaddis_8thEd_Chap12_Prob8_Array_File_Functions
+ * Author: Isaac Perez
+ * Created: 04/18/2026
+ * Purpose:  read and write binary files
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Open text and binary files
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
