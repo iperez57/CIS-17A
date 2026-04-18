c6ce2a8804ed40c9f0d07f6fac9a8c331436600e
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-18 14:07:51 -0700

    fills array with numbers

--- Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions.cpp
+++ Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions/Gaddis_8thEd_Chap12_Prob8_Array_File_Functions.cpp
@@ -18,25 +18,49 @@ using namespace std;
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
+void arrayToFile(string, int* , int);
+void fileToArray(string, int* , int);
+int* fillAry(const int);
 
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
 
-    //Open text and binary files
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    string name;
+    const int arySize = 5;
+    int* ary;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    ary = fillAry(arySize);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
 
+    delete[] ary;
     return 0;
 }
 
 //Function Implementations
+void arrayToFile(string fileName, int* ary, int arySize)
+{
+
+}
+
+void fileToArray(string fileName, int* ary, int arySize)
+{
+
+}
+
+int* fillAry(const int arySize)
+{
+    int* array = new int[arySize];
+    for (int i = 0; i < arySize; i++)
+    {
+        array[i] = i;
+    }
+    return array;
+}
