8c896ffe9fd3c85ecc48a766b7a0c465092ab049
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-17 19:20:32 -0700

    outline for chapter 11 problem 9 speakers bureau

---
+++ Hmwk/Code_E_Structures/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau/Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau.cpp
@@ -0,0 +1,39 @@
+/*
+ * File:   Gaddis_8thEd_Chap11_Prob9_Speakers_Bureau
+ * Author: Isaac Perez
+ * Created: 04/17/2026
+ * Purpose:  Use structures
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
