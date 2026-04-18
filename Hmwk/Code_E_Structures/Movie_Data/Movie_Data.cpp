22796cf124a3a200dd8c1542df4630b6ec3b048d
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-17 18:55:51 -0700

    gets data from user

--- Hmwk/Code_E_Structures/Movie_Data/Movie_Data.cpp
+++ Hmwk/Code_E_Structures/Movie_Data/Movie_Data.cpp
@@ -7,15 +7,23 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include <string>
 
 using namespace std;
 
 //User Libraries
-
+struct MovieData
+{
+    string Title;
+    string Director;
+    int yrReleased;
+    int rnTime;
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
+MovieData* getData(int arySize);
 
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
@@ -24,15 +32,33 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
+    int arySize;
+    MovieData* movies;
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    cin >> arySize;
+    movies = getData(arySize);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
+    delete[] movies;
     return 0;
 }
 
 //Function Implementations
+MovieData* getData(int arySize)
+{
+    MovieData* aryMovie = new MovieData[arySize];
+    for (int i = 0; i < arySize; i++)
+    {
+
+        getline(cin >> ws, aryMovie[i].Title);
+        getline(cin >> ws, aryMovie[i].Director);
+        cin >> aryMovie[i].yrReleased;
+        cin >> aryMovie[i].rnTime;
+    }
+
+    return aryMovie;
+}
