a45d8067d5191fd418113dfeb7cbc47c9c27c8dd
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-19 11:06:45 -0700

    outline for chap12 prob6 string search

---
+++ Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob6_String_Search/Gaddis_8thEd_Chap12_Prob6_String_Search.cpp
@@ -0,0 +1,41 @@
+/*
+ * File:   Gaddis_8thEd_Chap12_Prob6_String_Search
+ * Author: Isaac Perez
+ * Created: 04/19/2026
+ * Purpose:  read files
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <fstream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Open text and binary files
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
