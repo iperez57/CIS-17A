7d5da68a66fda722b8293e324a815e76289b7d87
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-19 08:25:16 -0700

    writes data into binary file, and checks for valid input from user for sales

--- Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob11_Corporate_Sales_Data_Output/Gaddis_8thEd_Chap12_Prob11_Corporate_Sales_Data_Output.cpp
+++ Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob11_Corporate_Sales_Data_Output/Gaddis_8thEd_Chap12_Prob11_Corporate_Sales_Data_Output.cpp
@@ -8,14 +8,16 @@
  //System Libraries
 #include <iostream>  //I/O Library
 #include <iomanip>
+#include <cstring>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 //User Libraries
 struct CompanyDiv
 {
-    string name;
+    char name[10];
     float q1;
     float q2;
     float q3;
@@ -26,6 +28,8 @@ struct CompanyDiv
 
 //Function Prototypes
 void getSales(CompanyDiv div[], string names[], int size);
+void writeFile(CompanyDiv div[], int size);
+
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -40,6 +44,7 @@ int main(int argc, char** argv) {
 
     //Display the Inputs/Outputs
     getSales(div, names, 4);
+    writeFile(div, 4);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
@@ -50,23 +55,62 @@ void getSales(CompanyDiv div[], string names[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        div[i].name = names[i];
+        strcpy_s(div[i].name, names[i].c_str());
 
         cout << div[i].name << endl;
 
         cout << "Enter first-quarter sales: ";
         cin >> div[i].q1;
+        while (div[i].q1 < 0)
+        {
+            cout << "Invalid enter again: ";
+            cin >> div[i].q1;
+        }
 
         cout << "Enter second-quarter sales: ";
         cin >> div[i].q2;
+        while (div[i].q2 < 0)
+        {
+            cout << "Invalid enter again: ";
+            cin >> div[i].q2;
+        }
 
         cout << "Enter third-quarter sales: ";
         cin >> div[i].q3;
+        while (div[i].q3 < 0)
+        {
+            cout << "Invalid enter again: ";
+            cin >> div[i].q3;
+        }
 
         cout << "Enter fourth-quarter sales: ";
         cin >> div[i].q4;
+        while (div[i].q4 < 0)
+        {
+            cout << "Invalid enter again: ";
+            cin >> div[i].q4;
+        }
+
 
         if (i < size - 1)
             cout << endl;
     }
+}
+
+void writeFile(CompanyDiv div[], int size)
+{
+    ofstream out("sales.bin", ios::binary);
+
+    if (!out)
+    {
+        cout << "Error opening file\n";
+        return;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        out.write(reinterpret_cast<char*>(&div[i]), sizeof(CompanyDiv));
+    }
+
+    out.close();
 }
