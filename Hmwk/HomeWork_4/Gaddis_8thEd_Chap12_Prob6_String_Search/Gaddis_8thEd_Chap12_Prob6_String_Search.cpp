d86633303edbe4863aa4f667de1fbccafddc0c8a
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-19 15:27:20 -0700

    searches file for occurence of a word

--- Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob6_String_Search/Gaddis_8thEd_Chap12_Prob6_String_Search.cpp
+++ Hmwk/HomeWork_4/Gaddis_8thEd_Chap12_Prob6_String_Search/Gaddis_8thEd_Chap12_Prob6_String_Search.cpp
@@ -8,6 +8,7 @@
  //System Libraries
 #include <iostream>  //I/O Library
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -18,6 +19,7 @@ using namespace std;
 
 //Function Prototypes
 void openFile(string);
+void searchFile(string&);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -31,7 +33,7 @@ int main(int argc, char** argv) {
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-    openFile(fileName);
+    searchFile(fileName);
 
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
@@ -40,13 +42,59 @@ int main(int argc, char** argv) {
 }
 
 //Function Implementations
-void openFile(string fileName)
+void searchFile(string& fileName)
 {
+    string searchStr;
+    string line;
+    int counter = 0;
+    int lineNumber = 0;
+    bool match;
+    bool foundInLine;
+
     ifstream inFile(fileName);
-    
+
     if (!inFile)
     {
         cout << "Error opening file" << endl;
         return;
     }
+
+
+    cout << "Enter a word to search for: ";
+    cin >> searchStr;
+
+    while (getline(inFile, line))
+    {
+        lineNumber++;
+        foundInLine = false;
+
+        if (line.length() >= searchStr.length())
+        {
+            for (int i = 0; i <= line.length() - searchStr.length(); i++)
+            {
+                match = true;
+
+                for (int j = 0; j < searchStr.length(); j++)
+                {
+                    if (line[i + j] != searchStr[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    counter++;
+                    foundInLine = true;
+                }
+            }
+        }
+        if (foundInLine)
+        {
+            cout << "Found in line " << lineNumber << ": " << line << endl;
+        }
+    }
+    cout << "Total occurences: " << counter << endl;
+    inFile.close();
 }
