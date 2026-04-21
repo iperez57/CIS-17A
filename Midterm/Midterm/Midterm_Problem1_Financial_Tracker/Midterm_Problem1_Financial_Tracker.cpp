de89e29e8608ac5c1d194d0be10b498c4a0fdf91
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-21 12:47:55 -0700

    gets all data info from customer

--- Midterm/Midterm/Midterm_Problem1_Financial_Tracker/Midterm_Problem1_Financial_Tracker.cpp
+++ Midterm/Midterm/Midterm_Problem1_Financial_Tracker/Midterm_Problem1_Financial_Tracker.cpp
@@ -7,15 +7,28 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include <string>
 using namespace std;
 
 //User Libraries
-
+struct Customer
+{
+    int accNum;
+    string name;
+    string address;
+    float begBalance;
+    //Dynamic Array
+    float* checks;
+    int checkCount;
+    //Dynamic Array
+    float* deposit;
+    int depoCount;
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-
+void getData(Customer*);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -23,15 +36,55 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    Customer* c = new Customer;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    getData(c);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
+    delete[] c->checks;
+    delete[] c->deposit;
+    delete c;
     return 0;
 }
 
 //Function Implementations
+void getData(Customer* c)
+{
+    do
+    {
+        cout << "Enter account number: ";
+        cin >> c->accNum;
+    } while (c->accNum < 10000 || c->accNum > 99999);
+
+    cout << "Enter name: ";
+    getline(cin >> ws, c->name);
+
+    cout << "Enter address: ";
+    getline(cin >> ws, c->address);
+
+    cout << "Enter beginning balance: $";
+    cin >> c->begBalance;
+
+    cout << "How many checks did you write? ";
+    cin >> c->checkCount;
+
+    c->checks = new float[c->checkCount];
+    for (int i = 0; i < c->checkCount; i++)
+    {
+        cout << "Enter amount for check " << i + 1 << ": ";
+        cin >> c->checks[i];
+    }
+
+    cout << "How many deposits did you make? ";
+    cin >> c->depoCount;
+
+    c->deposit = new float[c->depoCount];
+    for (int i = 0; i < c->depoCount; i++)
+    {
+        cout << "Enter amount for deposit " << i + 1 << ": ";
+        cin >> c->deposit[i];
+    }
+}
