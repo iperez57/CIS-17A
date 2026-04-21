3eb71a8e829de4aced4a1502c47ed32a98582a5f
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-21 13:54:23 -0700

    gets all data from user

--- Midterm/Midterm/Midterm_Problem2_Employee_Checks/Midterm_Problem2_Employee_Checks.cpp
+++ Midterm/Midterm/Midterm_Problem2_Employee_Checks/Midterm_Problem2_Employee_Checks.cpp
@@ -11,12 +11,17 @@
 using namespace std;
 
 //User Libraries
-
+struct Employee
+{
+    string name;
+    float hrsWorked;
+    float payRate;
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-
+void getData(Employee*, const int);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -24,16 +29,36 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    int numOfEmp;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
+    cout << "How many employees? ";
+    cin >> numOfEmp;
 
+    Employee* e = new Employee[numOfEmp];
+
+    getData(e, numOfEmp);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
-
+    delete[] e;
     return 0;
 }
 
 //Function Implementations
+void getData(Employee* e, const int numOfEmp)
+{
+    for (int i = 0; i < numOfEmp; i++)
+    {
+        cout << endl;
+        cout << "Employee #" << i + 1 << endl;
+        cout << "Name: ";
+        getline(cin >> ws, e[i].name);
+        cout << "Hours worked: ";
+        cin >> e[i].hrsWorked;
+        cout << "Pay rate: $";
+        cin >> e[i].payRate;
+
+    }
+}
