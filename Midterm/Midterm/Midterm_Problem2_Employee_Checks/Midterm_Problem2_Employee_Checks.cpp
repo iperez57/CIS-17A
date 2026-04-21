c0fb40645e70da6b6b1a27be3a217174d302b395
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-21 14:54:46 -0700

    adjusted roman numerals code to output in check format as well as added a teen number section

--- Midterm/Midterm/Midterm_Problem2_Employee_Checks/Midterm_Problem2_Employee_Checks.cpp
+++ Midterm/Midterm/Midterm_Problem2_Employee_Checks/Midterm_Problem2_Employee_Checks.cpp
@@ -17,6 +17,7 @@ struct Employee
     float hrsWorked;
     float payRate;
     float grossPay;
+    string grossPayWord;
 };
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
@@ -24,6 +25,7 @@ struct Employee
 //Function Prototypes
 void getData(Employee*, const int);
 void calculatePay(Employee*, const int);
+string numberToWords(float);
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
@@ -43,6 +45,7 @@ int main(int argc, char** argv) {
 
     getData(e, numOfEmp);
     calculatePay(e, numOfEmp);
+    
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     delete[] e;
@@ -85,4 +88,83 @@ void calculatePay(Employee* e, const int numOfEmp)
                 (e[i].hrsWorked - 50) * (e[i].payRate * 3);
         }
     }
+}
+
+string numberToWords(float n2Cnvrt)
+{
+    string result = "";
+
+    if (n2Cnvrt >= 1 && n2Cnvrt <= 3000)
+    {
+        //Process or map Inputs to Outputs
+        //Determine 1000's, 100's, 10's, 1's
+        unsigned char n1000s, n100s, n10s, n1s;
+        n1000s = n2Cnvrt / 1000;   //Shift 3 places to the left
+        n100s = n2Cnvrt % 1000 / 100;//Remainder of division of 1000 then shift 2 left
+        n10s = n2Cnvrt % 100 / 10;   //Remainder of division of 100 then shift 1 left
+        n1s = n2Cnvrt % 10;        //Remainder of division by 10
+
+        //Output the number of 1000's in Roman Numerals
+        //Using the Switch Statement
+        switch (n1000s) {
+        case 3: result += "Three thousand "; break;
+        case 2: result += "Two thousand "; break;
+        case 1: result += "One thousand "; break;
+        }
+
+        //Output the number of 100's
+        //Using the Ternary Operator
+        result += (n100s == 9 ? "nine hundred " :
+            n100s == 8 ? "eight hundred " :
+            n100s == 7 ? "seven hundred " :
+            n100s == 6 ? "six hundred " :
+            n100s == 5 ? "five hundred " :
+            n100s == 4 ? "four hundred " :
+            n100s == 3 ? "three hundred " :
+            n100s == 2 ? "two hundred " :
+            n100s == 1 ? "one hundred " : "");
+
+        //Output the number of 10's
+        //Using Independent If Statements
+        if (n10s == 1)
+        {
+            if (n1s == 0) result += "ten ";
+            else if (n1s == 1) result += "eleven ";
+            else if (n1s == 2) result += "twelve ";
+            else if (n1s == 3) result += "thirteen ";
+            else if (n1s == 4) result += "fourteen ";
+            else if (n1s == 5) result += "fifteen ";
+            else if (n1s == 6) result += "sixteen ";
+            else if (n1s == 7) result += "seventeen ";
+            else if (n1s == 8) result += "eighteen ";
+            else if (n1s == 9) result += "nineteen ";
+        }
+        //Output the number of 10's
+        //Using Independent If Statements
+        else
+        {
+            if (n10s == 9) result += "ninety ";
+            if (n10s == 8) result += "eighty ";
+            if (n10s == 7) result += "seventy ";
+            if (n10s == 6) result += "sixty ";
+            if (n10s == 5) result += "fifty ";
+            if (n10s == 4) result += "forty ";
+            if (n10s == 3) result += "thirty ";
+            if (n10s == 2) result += "twenty ";
+            if (n10s == 1) result += "ten ";
+        }
+
+        //Output the number of 1's
+        //Using Dependent If Statements
+        if (n1s == 9) result += "nine ";
+        else if (n1s == 8) result += "eight ";
+        else if (n1s == 7) result += "seven ";
+        else if (n1s == 6) result += "six ";
+        else if (n1s == 5) result += "five ";
+        else if (n1s == 4) result += "four ";
+        else if (n1s == 3) result += "three ";
+        else if (n1s == 2) result += "two ";
+        else if (n1s == 1) result += "one ";
+    }
+    return result;
 }
