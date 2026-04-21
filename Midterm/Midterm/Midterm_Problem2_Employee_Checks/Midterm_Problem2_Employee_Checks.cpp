349b03e252fabd44dab7308b7e2e4c531756341d
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-21 13:38:35 -0700

    outline for midterm problem 2 employee checks

---
+++ Midterm/Midterm/Midterm_Problem2_Employee_Checks/Midterm_Problem2_Employee_Checks.cpp
@@ -0,0 +1,39 @@
+/*
+ * File:   Midterm_Problem2_Employee_Checks
+ * Author: Isaac Perez
+ * Created: 04/21/2026
+ * Purpose:  use a structure
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
