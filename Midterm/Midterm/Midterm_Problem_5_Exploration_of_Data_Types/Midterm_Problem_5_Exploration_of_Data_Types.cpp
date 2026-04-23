5417817cd860ef3331c640048f76908015d5f23b
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-23 14:11:28 -0700

    prints out max factorial for each data type

--- Midterm/Midterm/Midterm_Problem_5_Exploration_of_Data_Types/Midterm_Problem_5_Exploration_of_Data_Types.cpp
+++ Midterm/Midterm/Midterm_Problem_5_Exploration_of_Data_Types/Midterm_Problem_5_Exploration_of_Data_Types.cpp
@@ -22,13 +22,26 @@ int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
-
+    
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
+    
+    cout << "Unsigned byte = !5" << endl;
+    cout << "Signed byte = !5" << endl;
+    cout << "Short = !7" << endl;
+    cout << "Unsigned short = !8" << endl;
+    cout << "Int = !12" << endl;
+    cout << "Unsigned int = !12" << endl;
+    cout << "Long = !12" << endl;
+    cout << "Unsigned long = !12" << endl;
+    cout << "Long long = !20" << endl;
+    cout << "Unsigned long long = !20" << endl;
+    cout << "Float = !34" << endl;
+    cout << "Double = !170" << endl;
 
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
