d617bfed3e2713c890d8f82574851e9cc6f34020
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-23 11:55:50 -0700

    outline for midterm probelm 4 encrypting and decrypting digits

---
+++ Midterm/Midterm/Midterm_Problem_4_Data_Encryption_Decryption/Midterm_Problem_4_Data_Encryption_Decryption.cpp
@@ -0,0 +1,39 @@
+/*
+ * File:   Midterm_Problem_4_Data_Encryption_Decryption
+ * Author: Isaac Perez
+ * Created: 04/23/2026
+ * Purpose:  swap array
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
