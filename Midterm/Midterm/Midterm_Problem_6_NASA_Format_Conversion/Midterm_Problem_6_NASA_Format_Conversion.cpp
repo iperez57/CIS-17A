9733757cc2663629200d88527852556e02559e5d
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-24 10:21:47 -0700

    outline for midterm problem 6 NASA conversion

---
+++ Midterm/Midterm/Midterm_Problem_6_NASA_Format_Conversion/Midterm_Problem_6_NASA_Format_Conversion.cpp
@@ -0,0 +1,39 @@
+/*
+ * File:   Midterm_Problem_6_Exploration_of_Data_Types
+ * Author: Isaac Perez
+ * Created: 04/24/2026
+ * Purpose:  NASA Format conversion
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
