d41ae8a9ad60dde03bb25e24a7cbffb586b67bc2
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-25 10:35:53 -0700

    takes in user input and passes into factor function

--- Midterm/Midterm/Midterm_Problem_7_Prime_Numbers/Midterm_Problem_7_Prime_Numbers.cpp
+++ Midterm/Midterm/Midterm_Problem_7_Prime_Numbers/Midterm_Problem_7_Prime_Numbers.cpp
@@ -29,9 +29,6 @@ struct Primes
 //Function Prototypes
 Primes* factor(int); //Factor the number into it's Primes
 void prtPrms(const Primes*); //Print the prime factors
-bool isPrime(int);  //Determine if the number is Prime
-int power(int, int&); //What power is this factor and remove from n
-int nPrimes(int); // How many primes
 void cleanUp(Primes*); //Clean it all up
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
@@ -40,12 +37,16 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    int num;
+    Primes* p;
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
+    cout << "Enter number: ";
+    cin >> num;
 
+    p = factor(num);
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
 
@@ -53,3 +54,31 @@ int main(int argc, char** argv) {
 }
 
 //Function Implementations
+Primes* factor(int num)
+{
+    Primes* result = new Primes;
+    result->n = num;
+    result->nPrimes = 0;
+    result->pStrAry = new Prime[20];
+
+    int n = num;
+    int count = 0;
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+            {
+                n /= i;
+                count++;
+            }
+
+            result->pStrAry[result->nPrimes].prime = i;
+            result->pStrAry[result->nPrimes].power = count;
+            result->nPrimes++;
+        }
+    }
+
+    return result;
+}
