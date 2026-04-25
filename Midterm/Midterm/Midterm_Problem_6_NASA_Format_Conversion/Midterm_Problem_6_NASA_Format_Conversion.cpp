acb3289c43b339deab4f3a1e76916f17e50a1856
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-25 10:11:23 -0700

    solutions for problem 6 printed

--- Midterm/Midterm/Midterm_Problem_6_NASA_Format_Conversion/Midterm_Problem_6_NASA_Format_Conversion.cpp
+++ Midterm/Midterm/Midterm_Problem_6_NASA_Format_Conversion/Midterm_Problem_6_NASA_Format_Conversion.cpp
@@ -29,7 +29,56 @@ int main(int argc, char** argv) {
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
+    cout << "49.1875:" << endl;
+    cout << "Binary: 110001.0011 " << endl;
+    cout << "Octal: 61.14" << endl;
+    cout << "Hex: 31.3" << endl;
+    cout << "NASA: 49 = 62000006, 0.1875 = 600000FE" << endl;
 
+    cout << "3.07421875:" << endl;
+    cout << "Binary: 11.00010011" << endl;
+    cout << "Octal: 3.046" << endl;
+    cout << "Hex: 3.16" << endl;
+    cout << "NASA: 3 = 60000002, 0.07421875 = 580000FD" << endl;
+
+    cout << "0.2:" << endl;
+    cout << "Binary: 0.00110011..." << endl;
+    cout << "Octal: 0.631463146..." << endl;
+    cout << "Hex: 0.19999..." << endl;
+    cout << "NASA: 0.2 = 199999FE" << endl;
+
+    cout << "-49.1875: "<< endl;
+    cout << "Binary: -0.1100010011 x 2^6" << endl;
+    cout << "Octal: -0.1423 x 8^6" << endl;
+    cout << "Hex: -0.1898 x 2^6" << endl;
+    cout << "NASA: 62600006" << endl;
+
+    cout << "-3.07421875:" << endl;
+    cout << "Binary: -0.110001011 x 2^2" << endl;
+    cout << "Octal: -0.1423 x 8^2" << endl;
+    cout << "Hex: -0.1898 x 2^2" << endl;
+    cout << "NASA: 62C00002" << endl;
+
+    cout << "-0.2" << endl;
+    cout << "Binary: -0.110011... x 2^-2" << endl;
+    cout << "Octal: -0.1463 x 8 ^-2/3" << endl;
+    cout << "Hex: -0.999x 2 ^ -2" << endl;
+    cout << "NASA: 666666FE" << endl;
+
+    cout << "69999902:" << endl;
+    cout << "E = 84" << endl;
+    cout << "1.60000002 " << endl;
+    cout << "1.60000002 x 2 ^ 84 " << endl;
+
+    cout << "69999903" << endl;
+    cout << "E = 84 " << endl;
+    cout << "1.600000023841858" << endl;
+    cout << "1.600000023841858 x 2 ^ 84 " << endl;
+
+    cout << "966667FE" << endl;
+    cout << "E = -83 " << endl;
+    cout << "-1.59999996 " << endl;
+    cout << "-1.59999996 x 2 ^ -83 " << endl;
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
 
