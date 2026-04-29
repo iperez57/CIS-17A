0696345cde357d2c9e7e06b1bfa357700e17d0a6
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-04-29 10:29:41 -0700

    Initializes a deck of cards

--- Project_1/BlackJack/BlackJack.cpp
+++ Project_1/BlackJack/BlackJack.cpp
@@ -14,7 +14,7 @@ using namespace std;
 struct Card
 {
     string rank;
-    string suite;
+    string suit;
     int value;
 };
 
@@ -36,12 +36,15 @@ struct Deck
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
-
+Deck initializeDeck();
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
+    Deck deck;
+    Player player;
+    Player dealer;
 
     //Initialize all known variables
 
@@ -49,7 +52,7 @@ int main(int argc, char** argv) {
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    deck = initializeDeck();
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
 
@@ -57,3 +60,35 @@ int main(int argc, char** argv) {
 }
 
 //Function Implementations
+Deck initializeDeck()
+{
+    Deck d;
+
+    string ranks[] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+    string suits[] = { "Hearts", "Spades", "Clubs", "Diamonds" };
+    int values[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+
+    d.cards = new Card[52];
+    d.size = 52;
+    d.topCard = 0;
+
+    int indx = 0;
+
+    const int NUM_SUITS = 4;
+    const int NUM_RANKS = 13;
+
+    //Initializes deck
+    for (int i = 0; i < NUM_SUITS; i++)
+    {
+        for (int j = 0; j < NUM_RANKS; j++)
+        {
+            d.cards[indx].suit = suits[i];
+            d.cards[indx].rank = ranks[j];
+            d.cards[indx].value = values[j];
+            
+            indx++;
+        }
+    }
+
+    return d;
+}
