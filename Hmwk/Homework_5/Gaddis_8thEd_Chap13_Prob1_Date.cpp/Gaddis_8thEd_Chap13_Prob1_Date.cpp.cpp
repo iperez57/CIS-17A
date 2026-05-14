77af976b3b62e820fa506769212c838797835f90
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-14 12:37:55 -0700

    numeric format mm/dd/yyyy class functin created and displays.

--- Hmwk/Homework_5/Gaddis_8thEd_Chap13_Prob1_Date.cpp/Gaddis_8thEd_Chap13_Prob1_Date.cpp.cpp
+++ Hmwk/Homework_5/Gaddis_8thEd_Chap13_Prob1_Date.cpp/Gaddis_8thEd_Chap13_Prob1_Date.cpp.cpp
@@ -7,12 +7,28 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include <iomanip>
 
 
 using namespace std;
 
 //User Libraries
+class Date
+{
+private:
+    int month;
+    int day;
+    int year;
+public:
 
+    // mm/dd/yyyy
+    void numericFormat(int, int, int);
+    // month dd, yyyy
+    void writtenFormat(int, int, int);
+    // dd, month, yyyy
+    void customFormat(int, int, int);
+
+};
 //Global Constants Only
 //Well known Science, Mathematical and Laboratory Constants
 
@@ -23,13 +39,25 @@ int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
-
+    int m;
+    int d;
+    int y;
+    Date date;
+    
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
+    cout << "enter month" << endl;
+    cin >> m;
+    cout << "enter day" << endl;
+    cin >> d;
+    cout << "enter year" << endl;
+    cin >> y;
+
+    date.numericFormat(m, d, y);
 
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
@@ -37,3 +65,9 @@ int main(int argc, char** argv) {
 }
 
 //Function Implementations
+void Date::numericFormat(int m, int d, int y)
+{
+    cout << endl;
+    cout << "mm/dd/yyyy" << endl;
+    cout << setfill('0') << setw(2) << m << "/" << setfill('0') << setw(2) << d << "/" << y << endl;
+}
