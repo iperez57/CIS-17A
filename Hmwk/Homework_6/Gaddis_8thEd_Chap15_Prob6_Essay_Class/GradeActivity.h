b779483ba9d06e94a7d0acdd7aa4bf4454ffdc0f
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-22 11:09:50 -0700

    copied grade activity class from chapter 15

---
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/GradeActivity.h
@@ -0,0 +1,43 @@
+/*
+ * File:   GradeActivity.h
+ * Author: Isaac Perez
+ * Created: 05/22/2026
+ * Purpose:  use Grade activity class from this chapter
+ */
+
+#ifndef GRADEACTIVITY_H
+#define GRADEACTIVITY_H
+
+//GradeActivity class declaration
+class GradeActivity
+{
+protected:
+	double score; // To hold the numeric score
+public:
+	//Default constructor
+	GradeActivity()
+	{
+		score = 0.0;
+	}
+
+	//Constructor
+	GradeActivity(double s)
+	{
+		score = s;
+	}
+
+	//Mutator function
+	void setScore(double s)
+	{
+		score = s;
+	}
+
+	//Accessor functions
+	double getScore() const
+	{
+		return score;
+	}
+
+	virtual char getLetterGrade() const;
+};
+#endif
