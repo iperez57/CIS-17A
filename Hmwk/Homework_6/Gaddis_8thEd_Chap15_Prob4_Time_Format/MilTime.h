556ca21c042b0d52ddbc120a18d62bb41b5cf92a
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-22 10:36:38 -0700

    MilTime class created

---
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob4_Time_Format/MilTime.h
@@ -0,0 +1,51 @@
+/*
+ * File:   MilTime.h
+ * Author: Isaac Perez
+ * Created: 05/22/2026
+ * Purpose:  use class inheritance
+ */
+
+#ifndef MILTIME_H
+#define MILTIME_H
+#include "Time.h"
+
+class MilTime : public Time
+{
+private:
+	int milHours;
+	int milSeconds;
+public:
+	MilTime(int, int);
+
+	void setTime(int, int);
+
+	int getMilHour();
+	int getStandHr();
+};
+MilTime::MilTime(int mH, int mS)
+{
+	setTime(mH, mS);
+}
+
+void MilTime::setTime(int mH, int mS)
+{
+	if (hour > 12)
+	{
+		hour -= 12;
+	}
+	hour = mH;
+	min = milHours % 100;
+	sec = mS;
+
+}
+int MilTime::getMilHour()
+{
+	return milHours;
+}
+
+int MilTime::getStandHr()
+{
+	return hour;
+}
+
+#endif
