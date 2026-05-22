e2da4d222ffdd535d81373768e237e7fa85d7457
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-22 10:04:57 -0700

    outputs all variables from production worker

--- Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob1_Employee_ProductionWorker_Classes/Gaddis_8thEd_Chap15_Prob1_Employee_ProductionWorker_Classes.cpp
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob1_Employee_ProductionWorker_Classes/Gaddis_8thEd_Chap15_Prob1_Employee_ProductionWorker_Classes.cpp
@@ -7,6 +7,7 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include "ProductionWorker.h"
 
 using namespace std;
 
@@ -22,14 +23,18 @@ int main(int argc, char** argv) {
     //Set the random number seed here
 
     //Declare all variables for this function
-
+    ProductionWorker prod("John Doe", "Jan 04 04", 12345, 1, 13.50);
     //Initialize all known variables
 
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    cout << " Name: " << prod.getName() << endl;
+    cout << " Hire date: " << prod.getDate() << endl;
+    cout << " Employee ID: " << prod.getNumber() << endl;
+    cout << " Shift: " << prod.getShift() << endl;
+    cout << " Pay rate: " << prod.getPayRate() << endl;
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
