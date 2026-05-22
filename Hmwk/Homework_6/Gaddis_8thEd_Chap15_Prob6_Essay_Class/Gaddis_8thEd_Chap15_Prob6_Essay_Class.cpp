b7e041bb7d27314623d62f88263936caaffa6e86
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-22 11:00:56 -0700

    outline for chapter 15 problem 6 essay class

---
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/Gaddis_8thEd_Chap15_Prob6_Essay_Class.cpp
@@ -0,0 +1,36 @@
+/*
+ * File:   Gaddis_8thEd_Chap15_Prob6_Essay_Class.cpp
+ * Author: Isaac Perez
+ * Created: 05/22/2026
+ * Purpose:  use class
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
