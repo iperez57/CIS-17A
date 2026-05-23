87ccf825e76f09ecc93b80aeae0ea8a1a74edcfb
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-23 08:48:54 -0700

    outline for chapter 16 problem 1 date exceptions

---
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap16_Prob1_Date_Exceptions/Gaddis_8thEd_Chap16_Prob1_Date_Exceptions.cpp
@@ -0,0 +1,99 @@
+/*
+ * File:   Gaddis_8thEd_Chap16_Prob1_Date_Exceptions.cpp
+ * Author: Isaac Perez
+ * Created: 05/23/2026
+ * Purpose:  use exceptions
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+#include <iomanip>
+#include <string>
+
+
+using namespace std;
+
+//User Libraries
+class Date
+{
+private:
+    int month;
+    int day;
+    int year;
+
+public:
+    string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September",  "October", "November", "December" };
+    // mm/dd/yyyy
+    void numericFormat(int, int, int);
+    // month dd, yyyy
+    void writtenFormat(int, int, int);
+    // dd month yyyy
+    void customFormat(int, int, int);
+
+};
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+    int m;
+    int d;
+    int y;
+    Date date;
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    do
+    {
+        cout << "enter month" << endl;
+        cin >> m;
+    } while (m < 1 || m > 12);
+
+    do
+    {
+        cout << "enter day" << endl;
+        cin >> d;
+    } while (d < 1 || d > 31);
+
+    cout << "enter year" << endl;
+    cin >> y;
+
+    date.numericFormat(m, d, y);
+    date.writtenFormat(m, d, y);
+    date.customFormat(m, d, y);
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
+void Date::numericFormat(int m, int d, int y)
+{
+    cout << endl;
+    cout << "mm/dd/yyyy" << endl;
+    cout << setfill('0') << setw(2) << m << "/" << setfill('0') << setw(2) << d << "/" << y << endl;
+}
+
+void Date::writtenFormat(int m, int d, int y)
+{
+    cout << endl;
+    cout << "month dd, yyyy" << endl;
+    cout << months[m - 1] << " " << setfill('0') << setw(2) << d << ", " << y << endl;
+}
+
+void Date::customFormat(int m, int d, int y)
+{
+    cout << endl;
+    cout << "dd, month, yyyy" << endl;
+    cout << setfill('0') << setw(2) << d << " " << months[m - 1] << " " << y << endl;
+}
