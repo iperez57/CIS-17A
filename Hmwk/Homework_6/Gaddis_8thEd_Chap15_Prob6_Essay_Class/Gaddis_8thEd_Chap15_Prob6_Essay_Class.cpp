827f9397bba99a1ed288763231a807fd65bbbf4d
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-05-23 08:42:05 -0700

    added letter grade characters, and display grade of 2 students.

--- Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/Gaddis_8thEd_Chap15_Prob6_Essay_Class.cpp
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/Gaddis_8thEd_Chap15_Prob6_Essay_Class.cpp
@@ -7,7 +7,9 @@
 
  //System Libraries
 #include <iostream>  //I/O Library
+#include "Essay.h"
 
+using namespace std;
 //User Libraries
 
 //Global Constants Only
@@ -22,12 +24,14 @@ int main(int argc, char** argv) {
     //Declare all variables for this function
 
     //Initialize all known variables
-
+    Essay student(30, 20, 20, 30);
+    Essay student2(20, 20, 10, 20);
     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
 
     //Display the Inputs/Outputs
-
+    cout << "John: " << student.getScore() << " " << student.getLetterGrade() << endl;
+    cout << "Joe: " << student2.getScore() << " " << student2.getLetterGrade() << endl;
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
--- Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/GradeActivity.h
+++ Hmwk/Homework_6/Gaddis_8thEd_Chap15_Prob6_Essay_Class/GradeActivity.h
@@ -38,6 +38,18 @@ public:
 		return score;
 	}
 
-	virtual char getLetterGrade() const;
+	virtual char getLetterGrade() const
+	{
+		if (score >= 90)
+			return 'A';
+		else if (score >= 80)
+			return 'B';
+		else if (score >= 70)
+			return 'C';
+		else if (score >= 60)
+			return 'D';
+		else
+			return 'F';
+	}
 };
 #endif
