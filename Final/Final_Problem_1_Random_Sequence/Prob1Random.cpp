08635c7343fd82a6bc5578cbda383d3d7c875826
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-07 19:32:52 -0700

    getters and randFromSet function implemented

--- Final/Final_Problem_1_Random_Sequence/Prob1Random.cpp
+++ Final/Final_Problem_1_Random_Sequence/Prob1Random.cpp
@@ -1,15 +1,21 @@
 //Prob1Random.cpp
 
 #include "Prob1Random.h"
+#include <cstdlib>
 
 using namespace std;
 
-Prob1Random::Prob1Random(const char c, const char* k)
+Prob1Random::Prob1Random(const char n, const char* rndseq)
 {
-	nset = c;
+	nset = n;
 	set = new char[nset];
 	freq = new int[nset]();
 	numRand = 0;
+
+	for (int i = 0; i < nset; i++)
+	{
+		set[i] = rndseq[i];
+	}
 }
 
 Prob1Random::~Prob1Random()
@@ -20,5 +26,25 @@ Prob1Random::~Prob1Random()
 
 char Prob1Random::randFromSet()
 {
+	int index = rand() % nset;
 
+	freq[index]++;
+	numRand++;
+
+	return set[index];
+}
+
+int* Prob1Random::getFreq() const
+{
+	return freq;
+}
+
+char* Prob1Random::getSet() const
+{
+	return set;
+}
+
+int Prob1Random::getNumRand() const
+{
+	return numRand;
 }
