da7b15a32dca28be6819ae74d42ac3aaf4cd7f47
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-08 11:16:49 -0700

    fixed sorting issue, indexing was incorrect and fixed text file. Didnt have the extra blank line which was causing errors

--- Final/Final_Problem_2_All_Kinds_Of_Sorting/Prob2Sort.h
+++ Final/Final_Problem_2_All_Kinds_Of_Sorting/Prob2Sort.h
@@ -36,8 +36,53 @@ T* Prob2Sort<T>::sortArray(const T*, int, bool)
 }
 
 template<class T>
-T* Prob2Sort<T>::sortArray(const T* ch2p, int row, int col, int colSort, bool ascending)
+T* Prob2Sort<T>::sortArray(const T* ch2p, int rows, int cols, int colSort, bool ascending)
 {
+	index = new int[rows];
+	colSort = colSort - 1;
 
+	for (int i = 0; i < rows; i++)
+	{
+		index[i] = i;
+	}
+
+	for (int i = 0; i < rows - 1; i++)
+	{
+		for (int j = i + 1 ; j < rows; j++)
+		{
+			if (ascending)
+			{
+				if (ch2p[index[i] * cols + colSort] > ch2p[index[j] * cols + colSort])
+				{
+					int temp = index[i];
+					index[i] = index[j];
+					index[j] = temp;
+
+				}
+			}
+			else
+			{
+				if (ch2p[index[i] * cols + colSort] < ch2p[index[j] * cols + colSort])
+				{
+					int temp2 = index[i];
+					index[i] = index[j];
+					index[j] = temp2;
+				}
+			}
+
+		}
+	}
+
+	T* sorted = new T[rows * cols];
+
+	for (int i = 0;i < rows;i++) 
+	{
+		for (int j = 0;j < cols;j++)
+		{
+			sorted[i * cols + j] = ch2p[index[i] * cols + j];
+		}
+	}
+
+	return sorted;
 }
 #endif
