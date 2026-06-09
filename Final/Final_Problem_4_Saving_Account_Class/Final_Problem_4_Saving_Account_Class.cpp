6d9bd6fb0274a8e352f8f361ab615d186aa64917
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-09 12:03:01 -0700

    included srand in driver

--- Final/Final_Problem_4_Saving_Account_Class/Final_Problem_4_Saving_Account_Class.cpp
+++ Final/Final_Problem_4_Saving_Account_Class/Final_Problem_4_Saving_Account_Class.cpp
@@ -8,6 +8,8 @@
  //System Libraries
 #include <iostream>  //I/O Library
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -23,7 +25,7 @@ using namespace std;
 //Execution of Code Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed here
-
+    srand(time(0));
     //Declare all variables for this function
 
     //Initialize all known variables
