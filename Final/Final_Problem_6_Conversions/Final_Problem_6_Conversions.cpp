741f3b6cf258110c80767d189582c3b0765439cd
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-10 11:48:04 -0700

    final conversion

---
+++ Final/Final_Problem_6_Conversions/Final_Problem_6_Conversions.cpp
@@ -0,0 +1,69 @@
+/*
+ * File:   Final_Problem_6_Conversion
+ * Author: Isaac Perez
+ * Created: 06/10/2026
+ * Purpose:  conversions
+ */
+
+ //System Libraries
+#include <iostream>  //I/O Library
+
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    cout << " 5.75" << endl;
+    cout << "1. Binary: 101.11     Octal: 5.6      Hex: 5.C" << endl;
+    cout << "2. NASA: 0. 1011 1000 0000 0000 0000 0000 0000 0011" << endl;
+    cout << "B8000003" << endl;
+    cout << "3. 1466" << endl;
+    cout << "4. 40.243" << endl;
+    cout << "5. 0.100 0000 1011 1000 0000 0000 0000 0000" << endl;
+    cout << " 40B80000" << endl;
+    cout << endl;
+
+    cout << "0.9" << endl;
+    cout << "1. Binary: 0.11100110011...       Octal: 0.71463... Hex: 0.E666..." << endl;
+    cout << "2. NASA: 0. 1110 0110 0110 0110 0110 0110 0000 0000" << endl;
+    cout << "E6666600" << endl;
+    cout << "3. 58981" << endl;
+    cout << "4. 6.3000076" << endl;
+    cout << "5. 0.011 1111 0110 0110 0110 0110 0110 0110" << endl;
+    cout << "6. 3F666666" << endl;
+    cout << endl;
+
+    cout << "99.7" << endl;
+    cout << "1. Binary: 1100011.10110011...        Octal: 143.546314...      Hex: 63.b333..." << endl;
+    cout << "2. NASA: 0. 1100 0111 0110 0110 0110 0110 0000 0111" << endl;
+    cout << "C7666607" << endl;
+    cout << "3. 1672687555" << endl;
+    cout << "4. 697.9" << endl;
+    cout << "5. 0100 0010 1100 0111 0110 0110 0110 0110" << endl;
+    cout << "42C76666" << endl;
+
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+
+    return 0;
+}
+
+//Function Implementations
